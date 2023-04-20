1f7e5abaf2e38b4fc64e2a4361ed954b58c717af
JustinBelterKTC <126703289+JustinBelterKTC@users.noreply.github.com>
2023-04-20 09:59:56 -0500

    Update ssh_test.py

--- ssh_test.py
+++ ssh_test.py
@@ -1,2 +1,21 @@
-import os
-os.system("ssh root@192.168.5.67")
+import paramiko as pk
+
+def start_connection():
+  u_name = 'root'
+  pass = 'nagiosxi'
+  port = 22
+  r_ip = '192.168.5.67'
+  
+  myconn = paramiko.SSHClient()
+  myconn.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+  
+  session = myconn.connect(r_ip, username=u_name, password=pass, port=port)
+
+  remote_cmd = 'ip addr'
+  (stdin, stdout, stderr) = myconn.exec_command(remote_cmd)
+  print("{}".format(stdout.read()))
+  print("{}".format(type(myconn)))
+  myconn.close()
+  
+if __name__ == '__main__':
+  start_connection()
